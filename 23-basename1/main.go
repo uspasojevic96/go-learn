35c83c092c63a71f0056ec84e6202582159b1fd8
Uros Spasojevic <uspasojevic96@gmail.com>
2022-05-10 19:57:59 +0200

    23-basename1

---
+++ 23-basename1/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+	s := ""
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+}
