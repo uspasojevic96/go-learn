065f4558365f5c893dc7ad43505b44f0f69b70f6
Uros Spasojevic <uspasojevic96@gmail.com>
2022-05-10 19:58:09 +0200

    24-basename2

---
+++ 24-basename2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "strings"
+
+func main() {
+	s := ""
+	slash := strings.LastIndex(s, "/")
+	s = s[slash+1:]
+	if dot := strings.LastIndex(s, "."); dot >= 0 {
+		s = s[:dot]
+	}
+}
